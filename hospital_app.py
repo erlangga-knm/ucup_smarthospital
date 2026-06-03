4a9b3af082e634fd3fde62dfdb1fa8b6f53aa911
erlangga-knm <erlangga@kodingnext.com>
2026-06-03 11:38:49 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -118,23 +118,19 @@ with st.form("triage_form"):
     </div>
     """, unsafe_allow_html=True)
 
-    c1, c2, c3, c4 = st.columns(4)
+    c1, c2, c3 = st.columns(3)
     with c1:
         fever            = st.checkbox("🌡️  Fever")
         cough            = st.checkbox("🤧  Cough")
-    with c2:
         headache         = st.checkbox("🤕  Headache")
+    with c2:
         chest_pain       = st.checkbox("💔  Chest Pain")
-    with c3:
         stomach_pain     = st.checkbox("🤢  Stomach Pain")
         shortness_breath = st.checkbox("😮‍💨  Shortness of Breath")
-    with c4:
+    with c3:
         nausea_vomiting  = st.checkbox("🤮  Nausea / Vomiting")
         dizziness        = st.checkbox("😵  Dizziness")
-
-    c5, _, _, _ = st.columns(4)
-    with c5:
-        skin_rash = st.checkbox("🔴  Skin Rash")
+        skin_rash        = st.checkbox("🔴  Skin Rash")
 
     st.markdown("<br>", unsafe_allow_html=True)
 
